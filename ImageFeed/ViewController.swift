aef6ffe79bd6a2c68d09d9a90c8b4ec67b9f76af
vprorok <vprorok@ivi.ru>
2024-03-24 21:54:43 +0300

    second commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Владислав Пророк on 17.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
